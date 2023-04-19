ba88d57182a7224ee06d1d5692deb91b4ba9d85f
Brandon Nestor Reis <brandon.nestor.reis@gmail.com>
2023-04-18 21:39:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`)
+  }
+
+  return gifts
+}
+
+wrapGifts(gifts)
+
+function writeCards(names, event) {
+    const completedCards = []
+    for (let i = 0; i < names.length; i++) {
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return completedCards
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer--)
+    }
+}
